78ba7f321d8e8a3b5456d3f707b7522c00d4e8a5
Alex K <aklimovskikh@yandex.ru>
2024-11-11 21:31:10 +0300

    Add index.js in api

---
+++ api/index.js
@@ -0,0 +1,27 @@
+let a = 5;
+console.log("api", a);
+
+function makeRequest(url, options = {}) {
+  return fetch(url, options).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+
+    return response.text().then((text) => {
+      throw new Error(text);
+    });
+  });
+}
+
+function add(title, content) {
+  let body = new FormData();
+  body.append("title", title);
+  body.append("content", content);
+
+  return makeRequest(`https://mpaint.ru/api/articles.php`, {
+    method: "POST",
+    body,
+  });
+}
+
+add("title a", "content" + Math.random());
