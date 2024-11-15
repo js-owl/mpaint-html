0f1863a5076a37c73bf25478fa55f4e06716c08e
Alex K <aklimovskikh@yandex.ru>
2024-11-15 14:09:28 +0300

    Add GET[id] in articles.php

--- api/articles.php
+++ api/articles.php
@@ -1,70 +1,77 @@
 <?php
-$fname = "data/articles.txt";
-if(is_file($fname)){
-    $result = file_get_contents($fname);
-    echo "1<br>";
-    echo $result;
-} else {
-    echo 'error';
-}
+$id = $_GET["id"];
+$ar = array();
+$o1 = array("id" => 1, "t" => "ta"); $ar['1'] = $o1;
+$o2 = array("id" => 2, "t" => "ta"); $ar['2'] = $o2;
+$res = json_encode($ar[$id]);
+echo $res;
 
-$articles = json_decode(file_get_contents('data/articles.txt'), true);
-echo "2<br>";
-print_r ($articles);
-// $articlesAI = (int)file_get_contents('data/ai.txt');
+// $fname = "data/articles.txt";
+// if(is_file($fname)){
+//     $result = file_get_contents($fname);
+//     echo "1<br>";
+//     echo $result;
+// } else {
+//     echo 'error';
+// }
+
+// $articles = json_decode(file_get_contents('data/articles.txt'), true);
+// echo "2<br>";
+// print_r ($articles);
+// // $articlesAI = (int)file_get_contents('data/ai.txt');
 
 
-switch($_SERVER['REQUEST_METHOD']){
-    case 'GET':
-        if(isset($_GET['id'])){
-            if(isset($articles[$_GET['id']])){
-                $res = $articles[$_GET['id']];
-            } else {
-                header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-            }
-        } else {
-            $res = array_values($articles);
-        }
-        break;
-    case 'POST':
-        echo "3<br>";
-        // $articles[++$articlesAI] = [
-        //     'id' => $articlesAI,
-        //     'title' => $_POST['title'],
-        //     'content' => $_POST['content'],
-        //     'dt' => date('Y-m-d H:i:s')
-        // ];
-        $o2 = array("id"=>2, "title"=>"tb", "content"=>"cb");
-        $articles["2"] = $o2;
-        print_r ($articles);
+// switch($_SERVER['REQUEST_METHOD']){
+//     case 'GET':
+//         if(isset($_GET['id'])){
+//             if(isset($articles[$_GET['id']])){
+//                 $res = $articles[$_GET['id']];
+//             } else {
+//                 header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+//             }
+//         } else {
+//             $res = array_values($articles);
+//         }
+//         break;
+//     case 'POST':
+//         echo "3<br>";
+//         // $articles[++$articlesAI] = [
+//         //     'id' => $articlesAI,
+//         //     'title' => $_POST['title'],
+//         //     'content' => $_POST['content'],
+//         //     'dt' => date('Y-m-d H:i:s')
+//         // ];
+//         $o2 = array("id"=>2, "title"=>"tb", "content"=>"cb");
+//         $articles["2"] = $o2;
+//         print_r ($articles);
 
-        // file_put_contents('data/ai', $articlesAI);
-        // $res = $articlesAI;
-        break;
-    case 'PUT':
-        $str = file_get_contents('php://input');
-        $res = $str;
+//         // file_put_contents('data/ai', $articlesAI);
+//         // $res = $articlesAI;
+//         break;
+//     case 'PUT':
+//         $str = file_get_contents('php://input');
+//         $res = $str;
 
-        $put = json_decode($str, true);
-        $res = $put['id'];
+//         $put = json_decode($str, true);
+//         $res = $put['id'];
 
-        if(isset($articles[$put['id']])){
-            $articles[$put['id']]['title'] = $put['title'];
-            $articles[$put['id']]['content'] = $put['content'];
-        } else {
-            $res = 'articles not found';
-        }
-        break;
-    case 'DELETE':
-        if(isset($_GET['id']) && isset($articles[$_GET['id']])){
-            unset($articles[$_GET['id']]);
-            $res = true;
-        } else {
-            $res = "can't remove articl";
-        }
-        break;
-    default:
-        $res = 'incorrect HTTP method';
-}
-file_put_contents('data/articles.txt', json_encode($articles));
+//         if(isset($articles[$put['id']])){
+//             $articles[$put['id']]['title'] = $put['title'];
+//             $articles[$put['id']]['content'] = $put['content'];
+//         } else {
+//             $res = 'articles not found';
+//         }
+//         break;
+//     case 'DELETE':
+//         if(isset($_GET['id']) && isset($articles[$_GET['id']])){
+//             unset($articles[$_GET['id']]);
+//             $res = true;
+//         } else {
+//             $res = "can't remove articl";
+//         }
+//         break;
+//     default:
+//         $res = 'incorrect HTTP method';
+// }
+// file_put_contents('data/articles.txt', json_encode($articles));
 // echo json_encode($res);
