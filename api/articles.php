4b2612052048650c291033ce6c83907c8841b2a2
Alex K <aklimovskikh@yandex.ru>
2024-11-17 14:32:52 +0300

    Clean articles.php

--- api/articles.php
+++ api/articles.php
@@ -1,4 +1,5 @@
 <?php
+// Начальное заполнение db.json, нужно чтоб понять какая структура файла записывается
 // $ar = array();
 // $o1 = array("id" => 1, "t" => "ta"); $ar['1'] = $o1;
 // $o2 = array("id" => 2, "t" => "tb"); $ar['2'] = $o2;
@@ -7,6 +8,8 @@
 // ---------------------------------------------------------------
 $f = file_get_contents('db.json');
 $articles = json_decode($f, true);
+// Файл autoinc хранит последнее id объекта. 
+// Если использовать count($articles) - тогда при удалении объекта, последний объект будет перезаписываться
 $len = (int)file_get_contents('autoinc.txt');
 
 switch($_SERVER['REQUEST_METHOD']){
@@ -52,48 +55,3 @@ switch($_SERVER['REQUEST_METHOD']){
 }
 file_put_contents('db.json', json_encode($articles));
 echo json_encode($res);
-
-// switch($_SERVER['REQUEST_METHOD']){
-
-//     case 'POST':
-//         echo "3<br>";
-//         // $articles[++$articlesAI] = [
-//         //     'id' => $articlesAI,
-//         //     'title' => $_POST['title'],
-//         //     'content' => $_POST['content'],
-//         //     'dt' => date('Y-m-d H:i:s')
-//         // ];
-//         $o2 = array("id"=>2, "title"=>"tb", "content"=>"cb");
-//         $articles["2"] = $o2;
-//         print_r ($articles);
-
-//         // file_put_contents('data/ai', $articlesAI);
-//         // $res = $articlesAI;
-//         break;
-//     case 'PUT':
-//         $str = file_get_contents('php://input');
-//         $res = $str;
-
-//         $put = json_decode($str, true);
-//         $res = $put['id'];
-
-//         if(isset($articles[$put['id']])){
-//             $articles[$put['id']]['title'] = $put['title'];
-//             $articles[$put['id']]['content'] = $put['content'];
-//         } else {
-//             $res = 'articles not found';
-//         }
-//         break;
-//     case 'DELETE':
-//         if(isset($_GET['id']) && isset($articles[$_GET['id']])){
-//             unset($articles[$_GET['id']]);
-//             $res = true;
-//         } else {
-//             $res = "can't remove articl";
-//         }
-//         break;
-//     default:
-//         $res = 'incorrect HTTP method';
-// }
-// file_put_contents('data/articles.txt', json_encode($articles));
-// echo json_encode($res);
