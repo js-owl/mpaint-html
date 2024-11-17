a10d2c9312b78bcbecefa29615f3c4a2c7158130
Alex K <aklimovskikh@yandex.ru>
2024-11-17 11:49:29 +0300

    Add all in index.js

--- api/index.js
+++ api/index.js
@@ -1,40 +1,44 @@
 const url = "https://mpaint.ru/api/articles.php";
+(async () => {
+  const all_raw = await fetch(url);
+  const all = await all_raw.json();
+  console.log({ all });
+})();
+// function makeRequest(url, options = {}) {
+//   return fetch(url, options).then((response) => {
+//     if (response.status === 200) {
+//       return response.json();
+//     }
 
-function makeRequest(url, options = {}) {
-  return fetch(url, options).then((response) => {
-    if (response.status === 200) {
-      return response.json();
-    }
+//     return response.text().then((text) => {
+//       throw new Error(text);
+//     });
+//   });
+// }
 
-    return response.text().then((text) => {
-      throw new Error(text);
-    });
-  });
-}
+// function all() {
+//   console.log("|-all");
+//   return makeRequest(url);
+// }
 
-function all() {
-  console.log("|-all");
-  return makeRequest(url);
-}
+// function one(id) {
+//   console.log("|-one", { id });
+//   return makeRequest(`https://mpaint.ru/api/articles.php?id=${id}`);
+// }
 
-function one(id) {
-  console.log("|-one", { id });
-  return makeRequest(`https://mpaint.ru/api/articles.php?id=${id}`);
-}
+// function add(t) {
+//   console.log("|-add", { t });
+//   let body = new FormData();
+//   body.append("t", t);
 
-function add(t) {
-  console.log("|-add", { t });
-  let body = new FormData();
-  body.append("t", t);
+//   return makeRequest(url, {
+//     method: "POST",
+//     body,
+//   });
+// }
 
-  return makeRequest(url, {
-    method: "POST",
-    body,
-  });
-}
+// all().then(console.log).catch(console.warn);
+// one("1").then(console.log).catch(console.warn);
+// add("td").then(console.log).catch(console.warn);
 
-all().then(console.log).catch(console.warn);
-one("1").then(console.log).catch(console.warn);
-add("td").then(console.log).catch(console.warn);
-
-one("3").then(console.log).catch(console.warn);
+// one("3").then(console.log).catch(console.warn);
